4cb8cf70814796eac8e758618783904dc870f9cb
riya24kark <iamriyakarkhanis@gmail.com>
2024-09-14 14:35:13 +0000

    variable practice

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -4,8 +4,28 @@
 
 #include <iostream>
 
+int a,b = 5; //a and b are global variables 
+
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    bool my_flag;
+    a = 7;
+    my_flag = false;
+
+    std::cout<<"a = " <<a<< std::endl;
+
+    std::cout<<"b = " <<b<< std::endl;
+
+    std::cout<<"flag = " <<my_flag<< std::endl;
+
+    my_flag = true;
+    std::cout <<"flag = " << my_flag<< std::endl;
+    std::cout <<"a + b = "<< a + b <<std::endl;
+    std::cout <<"a - b = "<< a - b <<std::endl;
+    unsigned int pos;
+    pos = b - a;
+    std::cout <<"b - a = "<< pos <<std::endl; //unsigned ints find the twos complement of the number in binary
+
+    
     
     std::cout << std::endl << std::endl;
     return (0);
