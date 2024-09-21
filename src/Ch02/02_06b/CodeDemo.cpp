09fadf64855b660d7659ddc043b441bee7bdba9a
riya24kark <iamriyakarkhanis@gmail.com>
2024-09-21 04:02:02 +0000

    updated text

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,9 +3,20 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstdint>
 
+#define CAPACITY 5000//when symbol finds CAPS = will replace with 5000
+#define DEBUG //helps include or exclude definition lines
 int main(){
-    
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+#ifdef DEBUG //not indented with the code
+    std::cout<<"[About to perform the addition]"<<std::endl;
+#endif //use for debugging and includes code conditionally if a marco has been defined
+    large += small; 
+    std::cout<<"The large integer is "<<large <<std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
